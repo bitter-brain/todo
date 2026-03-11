8b8e53476d876105d03094a78551c05416e33518
Boris Alekseev <borisalekseev95@gmail.com>
2026-03-11 19:39:04 +0200

    use import.meta.env.DEV instead of custom env variable for backend switching

--- src/shared/api/tasks/index.js
+++ src/shared/api/tasks/index.js
@@ -2,6 +2,8 @@ import localAPI from './local'
 import serverAPI from './server'
 
 const isLocal = import.meta.env.DEV
+console.log('isLocal:', isLocal, import.meta.env.DEV)
+
 const tasksAPI = isLocal ? localAPI : serverAPI
 
 export default tasksAPI
